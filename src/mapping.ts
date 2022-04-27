861ad83f3f7b0b653b48b8530550e90569ea9f15
kndt <mnxx@protonmail.ch>
2022-04-27 11:19:55 +0100

    remove call handlers

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,26 +1,7 @@
 import { log } from '@graphprotocol/graph-ts'
 
-import { DeployPoolCall, DeployNewNftPool } from "../generated/PoolDeployer/PoolDeployer"
+import { DeployNewNftPool } from "../generated/PoolDeployer/PoolDeployer"
 import { PoolDeployerEntity, NftPoolEntity } from "../generated/schema"
-
-export function handleDeployPool(call: DeployPoolCall): void {
-  let id = call.transaction.hash.toHex();
-  let poolDeployer = PoolDeployerEntity.load(id);
-
-  log.info('handleDeployPool call.transaction.hash id {}', [id.toString()]);
-
-  log.info('handleDeployPool call.inputs.operator id {}', [call.inputs.operator.toHex().toString()]);
-
-  if (!poolDeployer) {
-    poolDeployer = PoolDeployerEntity.load(call.inputs.operator.toHex());
-  }
-
-  if (!poolDeployer) {
-    poolDeployer = new PoolDeployerEntity(call.inputs.operator.toHex());
-  }
-
-  poolDeployer.save();
-}
 
 export function handleDeployNewNftPool(event: DeployNewNftPool): void {
   let id = event.params.nftPool.toHex();
